b6601af8dbcba8fbc8474e77cccbf7a1c4a1f749
Chris <chris@appsynth.net>
2019-10-02 22:09:05 +0700

    - design for non-authorized state was implemented;

--- app/src/main/kotlin/com/drevnitskaya/instaclientapp/presentation/login/LoginActivity.kt
+++ app/src/main/kotlin/com/drevnitskaya/instaclientapp/presentation/login/LoginActivity.kt
@@ -3,7 +3,9 @@ package com.drevnitskaya.instaclientapp.presentation.login
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.view.View.*
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
 import com.drevnitskaya.instaclientapp.R
 import kotlinx.android.synthetic.main.activity_login.*
 
@@ -18,10 +20,20 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        loginRoot.systemUiVisibility =
+            SYSTEM_UI_FLAG_LAYOUT_STABLE or SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+        ViewCompat.setOnApplyWindowInsetsListener(loginRoot) { _, insets ->
+            insets
+        }
+        ViewCompat.setOnApplyWindowInsetsListener(loginBackgroundImg) { _, insets ->
+            insets
+        }
+
+
+
         initViews()
     }
 
     private fun initViews() {
-        loginPasswordLayout.isPasswordVisibilityToggleEnabled = true
     }
 }
--- app/src/main/kotlin/com/drevnitskaya/instaclientapp/presentation/splash/SplashActivity.kt
+++ app/src/main/kotlin/com/drevnitskaya/instaclientapp/presentation/splash/SplashActivity.kt
@@ -2,13 +2,14 @@ package com.drevnitskaya.instaclientapp.presentation.splash
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.drevnitskaya.instaclientapp.presentation.login.LoginActivity
 import com.drevnitskaya.instaclientapp.presentation.main.MainActivity
 
 class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        startActivity(MainActivity.getStartIntent(this))
+        startActivity(LoginActivity.getStartIntent(this))
         finish()
     }
 }
